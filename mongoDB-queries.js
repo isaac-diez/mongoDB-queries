5633d15d6c97b4c963e7fc047042a6b198bed78d
isaac-diez <isaac.diez@proton.me>
2025-02-21 18:03:18 +0100

    Feat: 28 queries finished

--- mongoDB-queries.js
+++ mongoDB-queries.js
@@ -19,15 +19,13 @@ db.restaurants.find({ borough: {$in: ["Staten Island","Queens","Bronx","Brooklyn
 db.restaurants.find({ borough: {$nin: ["Staten Island","Queens","Bronx","Brooklyn"]  }});
 db.restaurants.find({ "grades.score": { $lte: 10  }}  ,{ restaurant_id:1, name:1, borough:1, cuisine:1, _id:0 });
 db.restaurants.find({ $and: [{ name: { $regex: "fish", $options:"i"  }}, { $or: [{ cuisine: {$nin: ["American","Chinese"]} }, { name: { $regex: "^(?!.*Wil).+$" }} ]} ]}, { restaurant_id:1, name:1, borough:1, cuisine:1, _id:0 });
-
 db.restaurants.find({ grades: {$elemMatch: {grade: "A", score: 11, "date.$date:1407715200000"} } } , { restaurant_id:1, name:1, grades: {$elemMatch: {grade: "A", score: 11, "date.$date:1407715200000"} }, _id:0 });
-
 db.restaurants.find({ "grades.1.grade": "A", "grades.1.score": 9, "grades.1.date.$date": 1407715200000  }, { restaurant_id: 1, name: 1, "grades.1": 1, _id: 0  });
+db.restaurants.find({"address.coord.1": {$gt: 42, $lte: 52}}, { restaurant_id: 1, name: 1, "address.coord": 1, _id: 0  });
+db.restaurants.find().sort({ name: 1 });
+db.restaurants.find().sort({ name: -1 });
+db.restaurants.find({},{cuisine:1, borough:1, _id:0}).sort({cuisine: 1, borough:-1});
 
-
-Escriu una consulta per trobar el restaurant_id, name, adreça i ubicació geogràfica per a aquells restaurants on el segon element del array coord conté un valor que és més de 42 i fins a 52.
-Escriu una consulta per organitzar el nom dels restaurants en ordre ascendent juntament amb totes les columnes.
-Escriu una consulta per organitzar el nom dels restaurants en ordre descendent juntament amb totes les columnes.
 Escriu una consulta per organitzar el nom de la cuisine en ordre ascendent i pel mateix barri de cuisine. Ordre descendent.
 Escriu una consulta per saber totes les direccions que no contenen el carrer.
 Escriu una consulta que seleccionarà tots els documents en la col·lecció de restaurants on el valor del camp coord és Double.
