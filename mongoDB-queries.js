90fab7f52cf1767abd714d79c362746322df892d
isaac-diez <isaac.diez@proton.me>
2025-02-24 13:40:02 +0100

    Feat: added query 12 that was missing from previous commit.

--- mongoDB-queries.js
+++ mongoDB-queries.js
@@ -1,15 +1,15 @@
 db.restaurants.find().pretty();
 db.restaurants.find({},{restaurant_id:1, name:1, borough:1, cuisine:1});
 db.restaurants.find({},{restaurant_id:1, name:1, borough:1, cuisine:1,_id:0});
-db.restaurants.find({},{restaurant_id:1, name:1, borough:1, zipcode:1,_id:0});
 db.restaurants.find({},{restaurant_id:1, name:1, borough:1, "address.zipcode":1,_id:0});
 db.restaurants.find({borough:"Bronx"});
 db.restaurants.find({borough:"Bronx"}).limit(5);
 db.restaurants.find({borough:"Bronx"}).skip(5).limit(5);
-db.restaurants.find({"grades.score": {$gt: 10} });
+db.restaurants.find({"grades.score": {$gt: 90} });
 db.restaurants.find({grades: { $elemMatch: { score: { $gt: 80, $lt: 100 } } } });
 db.restaurants.find({"address.coord.0": { $lt: -95.754168 } });
-db.restaurants.find({"grades.score": {$gt: 70},"address.coord.0": { $lt: -65.754168 },"cuisine":{$ne:"American"}});
+db.restaurants.find({$and: [{"grades.score": {$gt: 70},"address.coord.0": { $lt: -65.754168 },"cuisine":{$ne:"American"} }] });
+db.restaurants.find({"grades.score": {$gt: 70},"address.coord.0": { $lt: -65.754168 },"cuisine":{$ne:"American "}});
 db.restaurants.find({"cuisine": {$ne:"American"}, "grades.grade" : "A", "borough":{$ne:"Brooklyn"}} );
 db.restaurants.find({ "cuisine": {$ne:"American"}, "grades.grade" : "A", "borough":{$ne:"Brooklyn"} }).sort({ cuisine: -1 });
 db.restaurants.find({ name: { $regex: "Wil"  }}  ,{ restaurant_id:1, name:1, borough:1, cuisine:1, _id:0 });
